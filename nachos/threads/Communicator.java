7e41849a2588d389f1b6a3c5b11255ef5593f57c
Jin Kwon <krnjin94@gmail.com>
2017-10-25 02:44:28 -0700

    still need some more on COMMUNICATOR

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -1,7 +1,7 @@
 package nachos.threads;
 
 import nachos.machine.*;
-
+import java.util.*;
 /**
  * A <i>communicator</i> allows threads to synchronously exchange 32-bit
  * messages. Multiple threads can be waiting to <i>speak</i>, and multiple
@@ -10,11 +10,25 @@ import nachos.machine.*;
  * be paired off at this point.
  */
 public class Communicator {
+
+        ArrayList<Integer> buf;
+        private Lock commLock;
+        private Condition2 speakCondition;
+        private Condition2 listenCondition;
+        //private Condition2 retCondition;
+        //private Integer buffer;
 	/**
 	 * Allocate a new communicator.
 	 */
 	public Communicator() {
-	}
+	
+          commLock = new Lock();
+
+          speakCondition = new Condition2(commLock);
+
+          listenCondition = new Condition2(commLock);
+          buf = new ArrayList<Integer>();
+        }
 
 	/**
 	 * Wait for a thread to listen through this communicator, and then transfer
@@ -27,7 +41,21 @@ public class Communicator {
 	 * @param word the integer to transfer.
 	 */
 	public void speak(int word) {
-	}
+          commLock.acquire();
+        
+          if(buf.size() == 0)
+          {
+            buf.add(0, word);
+          }
+          else
+          {
+            buf.add(buf.size(), word);
+          }
+
+          listenCondition.wake();
+          speakCondition.sleep();
+          commLock.release();
+        }
 
 	/**
 	 * Wait for a thread to speak through this communicator, and then return the
@@ -36,6 +64,74 @@ public class Communicator {
 	 * @return the integer transferred.
 	 */
 	public int listen() {
-		return 0;
-	}
+	
+          commLock.acquire();
+
+          while(buf.size()  == 0)
+          {
+            listenCondition.sleep();
+          }
+
+          speakCondition.wake();
+    
+          int retValue = buf.get(0);
+          
+          buf.remove(0);
+          
+          commLock.release();
+
+          return retValue;
+        
+        }
+
+    public static void commTest6() {
+      final Communicator com = new Communicator();
+      final long times[] = new long[4];
+      final int words[] = new int[2];
+      KThread speaker1 = new KThread( new Runnable () {
+        public void run() {
+          com.speak(4);
+          times[0] = Machine.timer().getTime();
+        }
+      });
+      speaker1.setName("S1");
+      KThread speaker2 = new KThread( new Runnable () {
+        public void run() {
+          com.speak(7);
+          times[1] = Machine.timer().getTime();
+        }
+      });
+      speaker2.setName("S2");
+      KThread listener1 = new KThread( new Runnable () {
+        public void run() {
+          times[2] = Machine.timer().getTime();
+          words[0] = com.listen();
+        }
+      });
+      listener1.setName("L1");
+      KThread listener2 = new KThread( new Runnable () {
+        public void run() {
+          times[3] = Machine.timer().getTime();
+          words[1] = com.listen();
+        }
+      });
+      listener2.setName("L2");
+                                                                                                                  
+      speaker1.fork(); speaker2.fork(); listener1.fork(); listener2.fork();
+      speaker1.join(); speaker2.join(); listener1.join(); listener2.join();
+                                                                                                                            
+      Lib.assertTrue(words[0] == 4, "Didn't listen back spoken word."); 
+      Lib.assertTrue(words[1] == 7, "Didn't listen back spoken word.");
+      Lib.assertTrue(times[0] > times[2], "speak() returned before listen() called.");
+      Lib.assertTrue(times[1] > times[3], "speak() returned before listen() called.");
+      System.out.println("commTest6 successful!");
+    }
+
+    // Invoke
+
+    public static void selfTest()
+    {
+      commTest6();
+    }
+
 }
