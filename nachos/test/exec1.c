f3236ea32656f6c7961fb2409db0c2f76f2b4df6
You Zhenghong <cs120fdl@ieng6-201.ucsd.edu>
2017-11-17 18:34:56 -0800

    modify exec1.c

--- nachos/test/exec1.c
+++ nachos/test/exec1.c
@@ -14,7 +14,7 @@ main (int argc, char *argv[])
     int pid;
     printf("%s\n", "ready to exec");
     pid = exec(prog, 0, 0);
-    printf("%d\n", pid);
+    printf("pid num is: %d\n", pid);
     // the exit status of this process is the pid of the child process
     exit (pid);
 }
