528ec23b589f0c986fd79eaf0e54e1ad7d68ae84
zhulian <327176483@qq.com>
2017-11-21 00:01:17 -0800

    some mysterious power make the code works

--- nachos/userprog/UserProcess.java
+++ nachos/userprog/UserProcess.java
@@ -173,35 +173,25 @@ public class UserProcess {
 
 		byte[] memory = Machine.processor().getMemory();
 
-//		// for now, just assume that virtual addresses equal physical addresses
-//		if (vaddr < 0 || vaddr >= memory.length)
-//			return 0;
+		// for now, just assume that virtual addresses equal physical addresses
+		if (vaddr < 0 || vaddr >= memory.length)
+			return 0;
 		
 		//added
 		int remain = length;
-//		while(remain > 0) {
+		int j = 0;
+		while(remain > 0 ) {
 			int vpn = vaddr / Processor.pageSize;
-			int ptr = vaddr % Processor.pageSize;
+			int ptr = vaddr - (vaddr / Processor.pageSize) * Processor.pageSize;
 			int ppn = pageTable[vpn].ppn;
-			pageTable[vpn].used = true;
-			pageTable[vpn].dirty = true;
-			int i = 0;
-			for(;remain > 0; ptr++) {
-				if (vpn >= pageTable.length || vpn < 0)
-					break;
-				if (!pageTable[vpn].valid)
-					break;
-				data[i+offset] = memory[ppn*Processor.pageSize + ptr];
+			for(int i = 0; i + ptr < Processor.pageSize && remain > 0; i++) {
+				data[j + offset] = memory[ppn*Processor.pageSize + i + ptr];
 				remain--;
 				vaddr++;
-				i++;
-				if (ptr == Processor.pageSize ){
-					ptr = 0;
-					vpn ++;
-					ppn = pageTable[vpn].ppn;
-					pageTable[vpn].used = true;
-				}
+				j++;
 			}
+		}
+
 		return length - remain;
 	}
 
@@ -237,35 +227,25 @@ public class UserProcess {
 
 		byte[] memory = Machine.processor().getMemory();
 
-//		// for now, just assume that virtual addresses equal physical addresses
-//		if (vaddr < 0 || vaddr >= memory.length)
-//			return 0;
+		// for now, just assume that virtual addresses equal physical addresses
+		if (vaddr < 0 || vaddr >= memory.length)
+			return 0;
 		
 		//added
 		int remain = length;
-//		while(remain > 0) {
+		int j = 0;
+		while(remain > 0 ) {
 			int vpn = vaddr / Processor.pageSize;
-			int ptr = vaddr % Processor.pageSize;
+			int ptr = vaddr - (vaddr / Processor.pageSize) * Processor.pageSize;
 			int ppn = pageTable[vpn].ppn;
-			pageTable[vpn].used = true;
-			pageTable[vpn].dirty = true;
-			int i = 0;
-			for(;remain > 0; ptr++) {
-				if (vpn >= pageTable.length || vpn < 0)
-					break;
-				if (!pageTable[vpn].valid)
-					break;
-				memory[ppn*Processor.pageSize + ptr] = data[i+offset];
+			for(int i = 0; i + ptr < Processor.pageSize && remain > 0; i++) {
+				memory[ppn*Processor.pageSize + i + ptr] = data[j + offset];
 				remain--;
 				vaddr++;
-				i++;
-				if (ptr == Processor.pageSize ){
-					ptr = 0;
-					vpn ++;
-					ppn = pageTable[vpn].ppn;
-					pageTable[vpn].used = true;
-				}
+				j++;
 			}
+		}
+
 		return length - remain;
 	}
 
